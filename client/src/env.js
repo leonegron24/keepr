6ee3af4650856dae29dc809c53f24d70674a7494
Leonardo Negron <leonegron24@gmail.com>
2025-10-28 15:30:04 -0600

    init

---
+++ client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:7045' : ''
+export const useSockets = false
+export const domain = 'dev-cxvcv5un0avbw1ix.us.auth0.com'
+export const clientId = 'Wj3fnWeTZsH9Z4p5mDs1eYbsAGpGaNAP'
+export const audience = 'https://auth.speedwagonfoundation.com'
