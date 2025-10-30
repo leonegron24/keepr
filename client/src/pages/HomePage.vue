fe9f7a4906e48c7c33ed012e5587a4af672c434c
Leonardo Negron <leonegron24@gmail.com>
2025-10-30 17:16:40 -0600

    header complete i think

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -6,22 +6,45 @@ import Login from '@/components/Login.vue';
 </script>
 
 <template>
-  <header>
-    <div class="row justify-content-end shadow">
+  <header class="position-relative shadow">
+    <div class="d-flex align-items-center justify-content-between p-3">
 
+      <!-- Left: Home & Create Buttons -->
+      <div class="d-flex align-items-center">
+        <!-- Home Button -->
+        <RouterLink :to="{ name: 'Home' }" class="me-3 text-black">
+          <h3 class="bordered-text border border-secondary bg-secondary m-0 px-3 py-1">
+            Home
+          </h3>
+        </RouterLink>
 
-      <!-- TITLE and Login -->
-      <div class="col-md-6 justify-content-between d-flex align-items-center">
-        <RouterLink :to="{ name: 'Home' }" class="m-2 text-black text-decoration-none">
-          <div class="bordered-text">
+        <!-- Create Button -->
+        <a class="text-decoration-none text-black btn py-2 px-3" data-bs-toggle="collapse" aria-controls="navbarText"
+          aria-expanded="false" aria-label="Toggle navigation" data-bs-target="#create-options">
+          <h3 class="m-0 d-flex align-items-center">
+            Create <span class="ms-2"><i class="mdi mdi-menu-down"></i></span>
+          </h3>
+        </a>
+      </div>
+
+      <!-- Center: Title -->
+      <div class="position-absolute start-50 translate-middle-x text-center">
+        <RouterLink :to="{ name: 'Home' }" class="text-black text-decoration-none">
+          <div class="bordered-text px-3 py-1">
             <p class="m-0">the<br>keepr<br>co.</p>
           </div>
         </RouterLink>
+      </div>
 
+      <!-- Right: Login/Profile -->
+      <div class="d-flex align-items-center">
         <Login />
       </div>
+
     </div>
   </header>
+
+
 </template>
 
 <style scoped lang="scss">
