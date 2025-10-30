0093144062896914699027844c07b3a65002b0f9
Leonardo Negron <leonegron24@gmail.com>
2025-10-30 16:46:37 -0600

    Title and login buttons

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,13 +1,41 @@
-<script setup >
+<script setup>
 import Example from '@/components/Example.vue';
+import Login from '@/components/Login.vue';
 
 
 </script>
 
 <template>
-  <Example />
+  <header>
+    <div class="row justify-content-end shadow">
+      <!-- TITLE and Login -->
+      <div class="col-md-6 justify-content-between d-flex align-items-center">
+        <RouterLink :to="{ name: 'Home' }" class="m-2 text-black text-decoration-none">
+          <div class="bordered-text">
+            <p class="m-0">the<br>keepr<br>co.</p>
+          </div>
+        </RouterLink>
+        <Login />
+      </div>
+    </div>
+  </header>
 </template>
 
 <style scoped lang="scss">
+.bordered-text {
+  display: inline-block;
+  /* makes the box fit the text width */
+  border: 2px solid #333;
+  /* border color & thickness */
+  border-radius: 12px;
+  /* rounded corners */
+  padding: 4px 8px;
+  line-height: 1;
+}
 
+header {
+  font-family: 'Marko One', sans-serif;
+  font-weight: 400;
+  font-style: normal;
+}
 </style>
