0a81a43b6750362d365d2026db4cc5432d5eaf54
Leonardo Negron <leonegron24@gmail.com>
2025-10-31 00:47:16 -0600

    modal opening and styling a lil

--- client/src/components/KeepModal.vue
+++ client/src/components/KeepModal.vue
@@ -1,19 +1,41 @@
 <script setup>
+import { AppState } from '@/AppState.js';
+import { computed } from 'vue';
+
+
+const keep = computed(() => AppState.activeKeep)
 
 </script>
 
 
 <template>
-    <div class="modal fade" data-bs-backdrop="static" data-bs-keyboard="false" id="keepModal" tabindex="-1"
+    <div class="modal fade " data-bs-backdrop="static" data-bs-keyboard="false" id="keepModal" tabindex="-1"
         aria-labelledby="myModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-xl">
-            <div class="modal-content container-fluid">
-                Hello, here is a modal
-                <button type="button" class="btn-close mx-2 p-0" data-bs-dismiss="modal" aria-label="Close"></button>
+        <div class="modal-dialog modal-xl modal-dialog-centered">
+            <!-- CONTENT START -->
+            <div class="modal-content container-fluid" v-if="keep">
+                <div class="row">
+                    <div class="col-6 p-0 modalImg" :style="{ backgroundImage: `url(${keep.img})` }">
+                    </div>
+                    <div class="col-6 p-2">
+                        <div class="text-end m-0">
+                            <button type="button" class="btn-close text-end" data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                        </div>
+                        Hi!
+                    </div>
+                </div>
             </div>
+            <!-- CONTENT END -->
         </div>
     </div>
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.modalImg {
+    background-position: center;
+    background-size: cover;
+    aspect-ratio: 1/1;
+}
+</style>
